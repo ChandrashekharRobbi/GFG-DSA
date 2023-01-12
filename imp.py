28cf482c62f60ea0a4aa28864742371a84c37851
chandrashekharrobbi <chandrashekarrobbi789@gmail.com>
2023-01-12 19:35:42 +0530

    changes

--- imp.py
+++ imp.py
@@ -1,7 +1,12 @@
 import pyperclip as pypc
 def convertinotlink(s):
-    desc = None
     print(s)
     print(f"7. [{s}](#{s.replace(' ','-')})")
     print(f"#### {s}")
     pypc.copy(f"#### {s}")
+    
+def linuxconvertinotlink(s):
+    print(s)
+    print(f"7. [{s}](#{s.replace(' ','-')})")
+    print(f"#### {s}")
+    # pypc.copy(f"#### {s}")
