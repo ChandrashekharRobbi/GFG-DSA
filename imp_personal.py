1ee8b2a500c39794783c4a516ae1b3a61363b360
Chandrashekhar Robbi <chandrashekarrobbi789@gmail.com>
2023-01-13 17:05:08 +0530

    Changes

--- imp_personal.py
+++ imp_personal.py
@@ -1,12 +1,19 @@
 import pyperclip as pypc
+import time
 def convertinotlink(s):
     print(s)
-    print(f"7. [{s}](#{s.replace(' ','-')})")
-    print(f"#### {s}")
+    comment = f"7. [{s}](#{s.replace(' ','-')})"
+    markdown = f"#### {s}"
+    print(comment)
+    print(markdown)
+    
+    pypc.copy(comment)
+    
+    time.sleep(0.5)
     pypc.copy(f"#### {s}")
     
 def linuxconvertinotlink(s):
     print(s)
     print(f"7. [{s}](#{s.replace(' ','-')})")
     print(f"#### {s}")
-    # pypc.copy(f"#### {s}")
+
