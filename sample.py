fbabe68d9e824e858cc1e9b658ecb8721311a315
Chandrashekhar Robbi <chandrashekarrobbi789@gmail.com>
2023-03-21 16:24:20 +0530

    changes

--- sample.py
+++ sample.py
@@ -6,7 +6,6 @@ def delete_cell():
     display(Javascript('''
         var cell_index = IPython.notebook.get_selected_index();
         var prev = cell_index - 1;
-        IPython.notebook.delete_cell(prev);
         console.log("Cell deleted");
         IPython.notebook.kernel.execute("prev = " + prev, 
                                          { iopub: { output: function(data) { console.log(data); }}});   
@@ -19,12 +18,18 @@ def delete_cell():
         print("Failed to retrieve previous cell index")
 
 
-
-    
-    
-#     def delete_cell():
+# def delete_cell():
 #     display(Javascript('''
 #         var cell_index = IPython.notebook.get_selected_index();
 #         var prev = cell_index - 1;
-#         IPython.notebook.delete_cell(prev);
+#         IPython.notebook.delete_cell(cell_index);
+#         console.log("Cell deleted");
+#         IPython.notebook.kernel.execute("prev = " + prev, 
+#                                          { iopub: { output: function(data) { console.log(data); }}});   
 #     '''))
+#     try:
+#         prev = get_ipython().user_ns['prev']
+#         time.sleep(10)
+#         print("Previous cell index:", prev)
+#     except KeyError:
+#         print("Failed to retrieve previous cell index")
