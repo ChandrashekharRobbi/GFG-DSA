7305c336c0aa3811bd22671ed46e0efeb200c135
Chandrashekhar Robbi <chandrashekarrobbi789@gmail.com>
2023-03-23 11:28:16 +0530

    changes

--- sample.py
+++ sample.py
@@ -46,7 +46,7 @@ class MyFunction:
             pypc.copy(markdown)
             self.arr.append(comment)
             print("Successfully added to the comment list")
-        if delete:
+        elif delete:
             self.delete_cell_prev()
 #             self.to_markdown()
 #             time.sleep(1)
